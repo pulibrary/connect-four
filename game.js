e12882bc51280c868c749df00b8e2f8560fb0014
zaymasood <zmasood@princeton.edu>
2021-06-17 13:08:34 -0400

    use slots array for mouseover and mouseout

--- game.js
+++ game.js
@@ -56,36 +56,36 @@ player2.turn = 'false';
 
 for (let i = 0; i < slots.length; i++) {
     const slot = slots[i];
-    const colNumber = parseInt(slot.innerHTML, 10);
+
+    // find last slot in current column
+    let lastSlotNum = i;
+    while ((lastSlotNum + 7) < 42) {
+      lastSlotNum += 7;
+    }
 
     slot.addEventListener('mouseover', () => {
-
-
-      for (let i = numRows; i > 0; i--) {
-        let row = document.querySelector(`.grid .row:nth-child(${i})`);
-        let slot = row.childNodes[colNumber * 2 + 1];
+      for (let i = lastSlotNum; i >= 0; i -= 7) {
+        let slot = slots[i];
         slot.style.border = '4px solid black';
       }
     });
 
     slot.addEventListener('mouseout', () => {
-
-      for (let i = numRows; i > 0; i--) {
-        let row = document.querySelector(`.grid .row:nth-child(${i})`);
-        let slot = row.childNodes[colNumber * 2 + 1];
+      for (let i = lastSlotNum; i >= 0; i -= 7) {
+        let slot = slots[i];
         slot.style.border = '2px solid black';
       }
     });
 
     slot.addEventListener('click', () => {
       if (player1.turn) {
-        if (findSlot(red, i)) {
+        if (fillSlot(lastSlotNum, red)) {
           switchBtns();
           player1.turn = false;
           player2.turn = true;
         }
       } else {
-        if (findSlot(yellow, i)) {
+        if (fillSlot(lastSlotNum, yellow)) {
           switchBtns();
           player2.turn = false;
           player1.turn = true;
@@ -119,7 +119,7 @@ function isFilled(slot) {
 **/
 function resetBoard() {
   for (let i = 0; i < slots.length; i++) {
-      const slot = slots[i];
+      let slot = slots[i];
       if (slot.classList.contains(red)) {
         slot.classList.remove(red);
       } else if (slot.classList.contains(yellow)) {
@@ -139,30 +139,18 @@ function resetBoard() {
  *
  * @example
  *
- *     findSlot(slot, 'bg-warning');
+ *     fillSlot(slot, 'bg-warning');
 **/
-function findSlot(color, i) {
-  console.log(i);
-  for (let j = i; j < 42; j += 7) {
-    let slot = slots[j];
+function fillSlot(lastSlotNum, color) {
+  for (let i = lastSlotNum; i >= 0; i -= 7) {
+    let slot = slots[i];
     if (!isFilled(slot)) {
-      if (j + 7 >= 42) {
-        return fillSlot(slot, color);
-      }
-    } else {
-      slot = slots[j - 7];
-      if (!isFilled(slot)) {
-        return fillSlot(slot, color);
-      }
-      return false;
+      numClicks++;
+      slot.classList.add(color);
+      return true;
     }
   }
-}
-
-function fillSlot(slot, color) {
-  numClicks++;
-  slot.classList.add(color);
-  return true;
+  return false;
 }
 
 /**
