704d3b27e28216268dcf63d8ce3a5212ebcea433
zaymasood <zmasood@princeton.edu>
2021-06-17 16:23:18 -0400

    create board class

--- js/Board.js
+++ js/Board.js
@@ -1,4 +1,28 @@
 class Board {
-  constructor() {
+  constructor(slots) {
+    this.slots = slots;
+  }
+
+
+  getSlot(i) {
+    return this.slots[i];
+  }
+
+  length() {
+    return this.slots.length;
+  }
+
+  /**
+   * Clears all filled slots.
+  **/
+  reset() {
+    for (let i = 0; i < this.slots.length; i++) {
+        let slot = this.slots[i];
+        if (slot.classList.contains(player1.color)) {
+          slot.classList.remove(player1.color);
+        } else if (slot.classList.contains(player2.color)) {
+          slot.classList.remove(player2.color);
+        }
+    }
   }
 }
