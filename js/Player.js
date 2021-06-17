1758c5dcba10d52d9328f6e6e683620c11f6aca0
zaymasood <zmasood@princeton.edu>
2021-06-17 15:12:03 -0400

    update player class

---
+++ js/Player.js
@@ -0,0 +1,22 @@
+class Player {
+  constructor(color) {
+    this.color = color;
+    this.wins = 0;
+  }
+
+  set turn(bool) {
+    this._turn = bool;
+  }
+
+  get turn() {
+    return this._turn;
+  }
+
+  updateWins() {
+    this.wins++;
+  }
+
+  getWins() {
+    return this.wins;
+  }
+}
