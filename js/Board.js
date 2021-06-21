7072b2cde9eb99bfb791be34a93d23d3fec268b2
zaymasood <zmasood@princeton.edu>
2021-06-20 22:57:56 -0400

    add class 'filled' for filled slots

--- js/Board.js
+++ js/Board.js
@@ -27,13 +27,13 @@ class Board {
         let slot = this.slots[i];
         slot.classList.remove(this.player1.color);
         slot.classList.remove(this.player2.color);
+        slot.classList.remove('filled');
     }
   }
 
   // determines if selected slot is filled.
   slotFilled(slot) {
-    return slot.classList.contains(this.player1.color) ||
-           slot.classList.contains(this.player2.color);
+    return slot.classList.contains('filled');
   }
 
   //
@@ -54,20 +54,18 @@ class Board {
   updateTurns() {
     for (let i = 0; i < playerBtns.length; i++) {
       let playerBtn = playerBtns[i];
-      if (playerBtn.classList.contains('d-none')) {
+      if (playerBtn.classList.contains('d-none'))
         playerBtn.classList.remove('d-none');
-      } else {
+      else
         playerBtn.classList.add('d-none')
-      }
     }
 
     this.player1.updateTurn();
     this.player2.updateTurn();
-    if (this.currentPlayer === this.player1) {
+    if (this.currentPlayer === this.player1)
       this.currentPlayer = this.player2;
-    } else {
+    else
       this.currentPlayer = this.player1;
-    }
   }
 
   // finds the lowest slot in a column that has not been filled and fill it.
@@ -87,6 +85,7 @@ class Board {
       } else {
         if (i - numColumns >= 0) {
           slots[i - numColumns].classList.add(color);
+          slots[i - numColumns].classList.add('filled');
           this.updateTurns();
         }
         return;
@@ -95,6 +94,7 @@ class Board {
       // if its the last row and its empty, u want to fill the slot
       if (i === lastSlotNum && !this.slotFilled(slot)) {
         slot.classList.add(color);
+        slot.classList.add('filled');
         this.updateTurns();
         return;
       }
